f0c42123c917d1cd327d645580697233dd7874b3
tanvirislamchayan <imdtanvir181@gmail.com>
2024-08-20 12:20:40 +0600

    Project Ready to go

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('delete/', views.delete_seasson, name='delete'),
+    path('find-info/', views.search_info, name='search'),
+    path('save-info/', views.save_data, name='save-info'),
+    path('student-info/<str:roll>/', views.student, name='student'),
+    path('update-info/<str:roll>/', views.update_info, name='update'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout')
+]
