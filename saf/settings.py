a0f8ed122e68189813ac6d68884e6a241f597992
Tanvir Islam <imdtanvir181@gmail.com>
2025-03-20 22:23:55 +0600

    updated settins.py

--- saf/settings.py
+++ saf/settings.py
@@ -94,7 +94,9 @@ DATABASES = {
         'HOST': 'localhost',  
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'init_command': "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci",
         }
     }
 }
